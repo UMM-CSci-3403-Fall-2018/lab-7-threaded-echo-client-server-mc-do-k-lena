98fa98576551f5eb8b4e0cce28fcb035691ae7ba
ahnafy <yasin011@morris.umn.edu>
2018-11-05 19:28:59 -0600

    add code from previous lab

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,28 +1,62 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+	private static final int portNumber = 6013;
 
 	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
-
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
+		String server;
+		// Use "127.0.0.1", i.e., localhost, if no server is specified.
+		if (args.length == 0) {
+			server = "127.0.0.1";
+		} else {
+			server = args[0];
 		}
-		System.out.flush();
+
+		try {
+			// Connect to the server
+			Socket socket = new Socket(server, portNumber);
+
+
+			// Get the output stream so we can read from standard input and sent it to the socket
+			// Get the input stream so we can print data from socket
+			OutputStream output = socket.getOutputStream();
+			InputStream input = socket.getInputStream();
+
+			// Write data from standard input to the server
+			// Print all the input we receive from the server
+			int keyboardByte;
+			int outputByte;
+			while ((keyboardByte = System.in.read()) != -1) {
+				output.write(keyboardByte);
+				output.flush();
+				outputByte = input.read();
+				System.out.write(outputByte);
+
+			}
+
+			// Flushes out last bytes
+			System.out.flush();
+
+
+
+			// Close the socket when we're done reading from it
+			socket.close();
+
+			// Provide some minimal error handling.
+		} catch (ConnectException ce) {
+			System.out.println("We were unable to connect to " + server);
+			System.out.println("You should make sure the server is running.");
+		} catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
+		}
 	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,5 +1,9 @@
 package echoserver;
 
+
+
+import java.net.*;
+import java.io.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,23 +11,43 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+	public static final int portNumber = 6013;
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
+	public static void main(String[] args) {
+		try {
+			// Start listening on the specified port
+			ServerSocket sock = new ServerSocket(portNumber);
+
+			// Initalize byte variable
+			int serverByte;
+			// Run forever, which is common for server style services
+			while (true) {
+				// Wait until someone connects, thereby requesting a date
+				Socket client = sock.accept();
+				System.out.println("Got a request!");
+
+				// Construct a input from the client
+				// and an output to the client
+				InputStream input = client.getInputStream();
+				OutputStream output = client.getOutputStream();
+
+				// Takes the input from the client and returns it to the client
+				while ((serverByte = input.read()) != -1) {
+					output.write(serverByte);
+					output.flush();
+				}
+
+				// just in case
+				output.flush();
+
+				// Close the client socket since we're done.
+				client.close();
 			}
+			// *Very* minimal error handling.
+		} catch (IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
 		}
 	}
 }
